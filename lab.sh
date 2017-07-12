40b302eef3ec44c9cf406b6e998ed6fade1511fd
LutzLange <llange@redhat.com>
2017-07-12 12:02:23 +0200

    olab.sh readded

---
+++ lab.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# do it again
+#
+
+time { 
+  # stop running TODO check before this
+  cdk stop 
+
+  # delete minishift vm
+  cdk delete
+
+  # empty out minishift config dir
+  test -d ~/.minishift && rm -rf ~/.minishift
+
+  # run new setup ( config dir and tools )
+  cdk setup-cdk
+
+  # install addons and set vm options
+  ~/git/cdk-labs/pre-start-script.sh
+
+  # start the new vm with all options TODO change registration to use VARS
+  cdk start --metrics --ocp-tag=v3.5 
+
+  # do things that are needed post start ( adjust master-config.yaml )
+  ~/git/cdk-labs/post-start-script.sh
+}
