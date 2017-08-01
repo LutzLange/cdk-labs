71dc957ff3f2725c11da6af523e3295ddadb9bc0
LutzLange <llange@redhat.com>
2017-08-01 14:32:47 +0200

    extended ansi-mac.sh

--- ansi-mac.sh
+++ ansi-mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # test Lab Setup with ansible an Mac OS
+#  Open ToDo Ansibleize
 #
 
 # kick off install of CMDLine dev tools
@@ -17,7 +18,21 @@ ansible --version &>/dev/null || sudo pip install ansible
 
 # Clone the repo
 test -d ~/git || mkdir ~/git
-cd ~/git
-git clone https://github.com/LutzLange/cdk-labs.git
+test -d ~/git/cdk-labs || { cd ~/git; git clone https://github.com/LutzLange/cdk-labs.git; }
+
+# Create bin folder
+test -d ~/bin || mkdir ~/bin
+
+# extend PATH
+echo ‘export PATH=$PATH:$HOME/bin’ >> ~/.bash_profile
+
+# Get CDK (ToDo official CDK when released)
+wget http://people.redhat.com/~llange/cdkmac -O ~/bin/cdk
+
+# Install homebrew
+brew --version || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install 
+brew list docker-machine-driver-xhyve || brew install docker-machine-driver-xhyve
 
 
