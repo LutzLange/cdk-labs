82df757b2fecfe290ba8ce90fd81efe1a2281f02
LutzLange <llange@redhat.com>
2017-08-01 15:36:35 +0200

    updated ansi-mac.sh

--- ansi-mac.sh
+++ ansi-mac.sh
@@ -13,6 +13,7 @@ You are running the OpenShift CDK Lab Installer for MacOS. The following actions
 4. Get the latest CDK ( currently nightly buids for 3.1 ) and put it in ~/bin
 5. Install Homebrew
 6. Install docker-machine-driver-xhyve
+7. Install olab command in ~/bin
 
 You will need to enter your password for prviledge actions.
 
@@ -39,10 +40,16 @@ sudo /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license acce
 #echo "Installing Ansible"
 #ansible --version &>/dev/null || sudo pip install ansible
 
-# Clone the repo
+# Clone / update the repo
 echo "2. Create ~/git directory and check out cdk-lab into ~/git/cdk-labs"
 test -d ~/git && echo "~/git is already therer" || mkdir ~/git
-test -d ~/git/cdk-labs && echo "cdk-labs is already checked out"|| { cd ~/git; git clone https://github.com/LutzLange/cdk-labs.git; }
+if test -d ~/git/cdk-labs then
+       echo "cdk-labs is already checked out, updating cdk-labs instead"
+       cd ~/git; git pull
+       cd
+else
+       cd ~/git; git clone https://github.com/LutzLange/cdk-labs.git
+fi
 
 # Create bin folder
 echo "3. Create ~/bin and extend your PATH to include ~/bin"
@@ -60,8 +67,25 @@ curl http://people.redhat.com/~llange/cdkmac -O ~/bin/cdk
 echo "5. Checking / Installing homebrew"
 brew --version &>/dev/null && echo "homebrew was installed" || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install 
+# Install docker-machine-driver-xhyve
 echo "6. Installing docker-machine-driver-xhyve"
 brew list docker-machine-driver-xhyve &>/dev/null && echo "xhyve was installed" || brew install docker-machine-driver-xhyve
+
+# Install the olab Command in ~/bin
+echo "7. Installing olab Script"
+cp ~/git/cdk-labs/olab ~/bin
+
+# You are ready to roll now
+cat <<ENDMESSAGE
+
+You are ready to check your environment and start the OpenShift environment.
+You will have all the necessary tools installed now. 
+
+Check your environment with : $ cdk version
+Build or reset your lab environment with : $ olab
+
+NOTE : you will need a fast internet connection to download docker images
+NOTE2 : 16GB RAM are currently required 
+ENDMESSAGE
 
 
