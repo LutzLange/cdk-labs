0b84fea688496cb2e781a3959a9fdc3fcbae5434
LutzLange <llange@redhat.com>
2017-08-03 10:34:38 +0200

    updated lab.sh

--- lab.sh
+++ lab.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 #
 # do it again
-#
 
+# specify -q as parameter for quick mode and skip intro
+QMODE="$1"
+
+intro () {
+
+	cat <<-EMSG
+	Welcome to the OpenShift CDK Lab.
+
+	Please Note :
+	This tool will setup the lab on your machine.
+	This will take ~15min at least.
+	This will clear out your existing ~/.minishift !
+
+	You can skip this intro by using Quick Mode with -q.
+
+	EMSG
+	read -p "Are you sure to continue? (Y/N) : " ANSWER  
+	test $ANSWER != Y && { echo "Found \"$ANSWER\" expecting \"Y\" Aborting Procedure now"; exit 1 ; }
+
+}
+
+# Skip or call the intro?
+test "$QMODE" != "-q" && { echo $QMODE; intro; }
+
+################################
+# This is where we do the work #
+################################
 time { 
   # stop running TODO check before this
   cdk stop --skip-unregistration
