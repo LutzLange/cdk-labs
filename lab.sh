cdf0920df8262a7e5d07e95dda9b2b33bdc7004e
LutzLange <llange@redhat.com>
2017-08-09 13:46:06 +0200

    moved pre_start_script to labs.sh pre_start_func

--- lab.sh
+++ lab.sh
@@ -4,6 +4,75 @@
 
 # specify -q as parameter for quick mode and skip intro
 QMODE="$1"
+OCP_VER="v3.5"
+
+# declare config hashmap
+declare -A config
+
+lab_part_one () {
+  ## Part 1
+  config["VMDISC"]=20G
+  config["MEM"]=$[4*1024]
+  config["REG"]=no
+  config["ADDONS"]="registry-console"
+  config["STACKS"]=""
+}
+
+lab_part_two () {
+  ## Part 2
+  config["VMDISC"]=40G
+  config["MEM"]=$[12*1024]
+  config["REG"]=no
+  config["ADDONS"]="registry-console cfme"
+  config["STACKS"]="--metrics"
+}
+
+lab_part_three () {
+  ## Part 3
+  config["VMDISC"]=40G
+  config["MEM"]=$[14*1024]
+  config["REG"]=yes
+  config["ADDONS"]="cns"
+  config["STACKS"]=""
+}
+
+full_lab () {
+  ## full setup
+  config["VMDISC"]=40G
+  config["MEM"]=$[16*1024]
+  config["REG"]=yes
+  config["ADDONS"]="registry-console cfme cns"
+  config["STACKS"]="--metrics"
+}
+
+pre_start_func () {
+  # replace pre_start_script
+  echo "Not implemented yet"
+  cd ~/git/cdk-labs/
+
+  cdk config set memory ${config["MEM"]}
+  cdk config set disk-size ${config["VMDISC"]}
+  #cdk config set image-caching true
+
+  for ADDON in ${config["ADDONS"]}
+  do
+	cdk addons install $ADDON
+	cdk addons enable $ADDON
+  done
+}
+
+###
+# initialize vars
+
+# TODO use getopts
+lab_part_two
+
+STOP_OPT=""
+START_OPT="--ocp-tag=$OCP_VER ${config["STACKS"]}"
+
+# set stop and start args
+test "${config["REG"]}" = "no" && STOP_OPT="$STOP_OPT --skip-unregistration"
+test "${config["REG"]}" = "no" && START_OPT="$START_OPT --skip-registration" 
 
 intro () {
 
@@ -31,22 +100,28 @@ test "$QMODE" != "-q" && { echo $QMODE; intro; }
 ################################
 time { 
   # stop running TODO check before this
-  cdk stop --skip-unregistration
+  echo test "$(cdk status)" = "Running" && cdk stop $STOP_OPT
 
   # delete minishift vm
   cdk delete
 
   # empty out minishift config dir
-  test -d ~/.minishift && rm -rf ~/.minishift
+  test -d ~/.minishift && { 
+    test -f ~/.minishift/cdk && rm -rf ~/.minishift || { 
+      echo "existing ~/.minishift moved to ~/minishift-saved"
+      mv ~/.minishift ~/minishift-saved 
+      }
+  }
 
   # run new setup ( config dir and tools )
   cdk setup-cdk
+  touch ~/.minishift/cdk
 
   # install addons and set vm options
-  ~/git/cdk-labs/pre-start-script.sh
+  pre_start_func ${config["ADDONS"]} 
 
   # start the new vm with all options 
-  cdk start --metrics --ocp-tag=v3.5 --skip-registration
+  cdk start $START_OPT
 
   # do things that are needed post start ( adjust master-config.yaml )
   ~/git/cdk-labs/post-start-script.sh
